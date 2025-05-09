d46815e1aa570d46d0abb61397dd49d1e93b2008
Hayoung Park <57648788+harris91@users.noreply.github.com>
2025-05-09 13:01:57 +0900

    [level 0] Title: 문자열 묶기, Time: 2.96 ms, Memory: 47.6 MB -BaekjoonHub

--- 프로그래머스/0/181855. 문자열 묶기/문자열 묶기.js
+++ 프로그래머스/0/181855. 문자열 묶기/문자열 묶기.js
@@ -1,5 +1,5 @@
 function solution(strArr) {
-    let arr = Array(strArr.length).fill(0);
+    let arr = Array(31).fill(0);
     strArr.forEach(str => arr[str.length] += 1);
     return arr.sort((a,b) => b-a).shift()
 }
